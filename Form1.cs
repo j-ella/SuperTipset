65ad19d8750153e8e62c60fe9bc57eebc16b9626
Ella <38213946+j-ella@users.noreply.github.com>
2025-08-18 14:48:06 +0200

    La till valda lag funktion

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,7 @@ namespace SuperTipset
         public frm_homepage()
         {
             InitializeComponent();
+            dgv_teamlist.RowHeadersVisible = false;
         }
 
         private void frm_homepage_Load(object sender, EventArgs e)
@@ -26,7 +27,7 @@ namespace SuperTipset
             this.sportTableAdapter.Fill(this.superTipsetDataSet.Sport);
 
         }
-        
+
         private void btn_add_Click(object sender, EventArgs e)
         {
             frm_newteams frm_Newteams = new frm_newteams();
@@ -39,25 +40,63 @@ namespace SuperTipset
             Application.Exit();
         }
         //Hämtar lag från vald sport i combobox och visar i datagridview
-        private void cmb_sport_SelectedIndexChanged(object sender, EventArgs e)
+         private void cmb_sport_SelectedIndexChanged(object sender, EventArgs e)
+         {
+             // Hämta sportens ID eller namn från ComboBoxen
+             int sportID = Convert.ToInt32 (cmb_sport.SelectedIndex+1);
+
+             // SQL-fråga för att hämta endast de lag som tillhör vald sport
+             string query = "SELECT * FROM Lag WHERE SportID = @Sport";
+             //string connectionstring = "Data Source=DESKTOP-S499K0O\\SQLEXPRESS;Initial Catalog=SuperTipset;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+             using (SqlConnection conn = new SqlConnection("Data Source=LEXICON\\SQLEXPRESS;Initial Catalog=SuperTipset;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"))
+             using (SqlCommand cmd = new SqlCommand(query, conn))
+             {
+                 cmd.Parameters.AddWithValue("@Sport", sportID);
+                 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                 DataTable dt = new DataTable();
+                 adapter.Fill(dt);
+
+                 // Sätt DataGridView till den filtrerade tabellen
+                 dgv_teamlist.DataSource = dt;
+             }
+         }
+        private void dgv_teamlist_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            // Hämta sportens ID eller namn från ComboBoxen
-            int sportID = Convert.ToInt32 (cmb_sport.SelectedIndex+1);
-
-            // SQL-fråga för att hämta endast de lag som tillhör vald sport
-            string query = "SELECT * FROM Lag WHERE SportID = @Sport";
-            //string connectionstring = "Data Source=DESKTOP-S499K0O\\SQLEXPRESS;Initial Catalog=SuperTipset;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-
-            using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-S499K0O\\SQLEXPRESS;Initial Catalog=SuperTipset;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"))
-            using (SqlCommand cmd = new SqlCommand(query, conn))
+            if (e.RowIndex >= 0) // undvik headern
             {
-                cmd.Parameters.AddWithValue("@Sport", sportID);
-                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                adapter.Fill(dt);
+                // Hämta lagets namn från databasen
+                string lagNamn = dgv_teamlist.Rows[e.RowIndex].Cells[0].Value.ToString();
 
-                // Sätt DataGridView till den filtrerade tabellen
-                dgv_teamlist.DataSource = dt;
+                // Kontrollera om laget redan är valt
+                if (txt_team1.Text == lagNamn || txt_team2.Text == lagNamn ||
+                    txt_team3.Text == lagNamn || txt_team4.Text == lagNamn)
+                {
+                    MessageBox.Show("Detta lag är redan valt.");
+                    return;
+                }
+
+                // Lägg in laget i första lediga textbox
+                if (string.IsNullOrEmpty(txt_team1.Text))
+                {
+                    txt_team1.Text = lagNamn;
+                }
+                else if (string.IsNullOrEmpty(txt_team2.Text))
+                {
+                    txt_team2.Text = lagNamn;
+                }
+                else if (string.IsNullOrEmpty(txt_team3.Text))
+                {
+                    txt_team3.Text = lagNamn;
+                }
+                else if (string.IsNullOrEmpty(txt_team4.Text))
+                {
+                    txt_team4.Text = lagNamn;
+                }
+                else
+                {
+                    MessageBox.Show("Du kan bara välja 4 lag.");
+                }
             }
         }
     }
